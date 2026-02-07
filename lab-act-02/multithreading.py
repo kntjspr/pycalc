2e002d7ac5518e4dd1c79c85555fe6906a68eaef
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-02-07 09:22:55 +0000

    updated the multithreading code for grade input

--- lab-act-02/multithreading.py
+++ lab-act-02/multithreading.py
@@ -1,16 +1,30 @@
 import threading
+import time
 
 def compute_gwa(grades):
     gwa = sum(grades) / len(grades)
     print(f"[Thread] Calculated GWA: {gwa}")
 
-grades_list = [85, 90, 78, 92] # Replace with user input
+if __name__ == "__main__":
+    grades_list = []
+    n = int(input("Enter number of subjects: "))
 
-threads = []
-for grade in grades_list:
-    t = threading.Thread(target=compute_gwa, args=([grade],))
-    threads.append(t)
-    t.start()
+    for i in range(n):
+        grade = float(input(f"Enter grade {i+1}: "))
+        grades_list.append(grade)
 
-for t in threads:
-    t.join()
+    threads = []
+
+    start_time = time.time()
+
+    for grade in grades_list:
+        t = threading.Thread(target=compute_gwa, args=([grade],))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    end_time = time.time()
+
+    print("Multithreading Execution Time:", end_time - start_time)
