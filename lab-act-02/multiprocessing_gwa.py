fdf6f0f8825efd42cab59d343f527d90b55fbcc2
jars-ofclay <lerio.jarschristian7@gmail.com>
2026-02-07 17:05:32 +0800

    Add multiprocessing implementation for GWA calculation

---
+++ lab-act-02/multiprocessing_gwa.py
@@ -0,0 +1,30 @@
+from multiprocessing import Process
+import time
+
+def compute_gwa_mp(grades):
+    gwa = sum(grades) / len(grades)
+    print(f"[Process] Calculated GWA: {gwa}")
+
+if __name__ == "__main__":
+    grades_list = []
+    n = int(input("Enter number of subjects: "))
+
+    for i in range(n):
+        grade = float(input(f"Enter grade {i+1}: "))
+        grades_list.append(grade)
+
+    processes = []
+
+    start_time = time.time()
+
+    for grade in grades_list:
+        p = Process(target=compute_gwa_mp, args=([grade],))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    end_time = time.time()
+
+    print("Multiprocessing Execution Time:", end_time - start_time)
