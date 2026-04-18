40fb3ec55b914037befe950b7a1252d9a7018741
geraldnofuckstogive666 <boniel.gerald666@gmail.com>
2026-04-18 21:11:11 +0800

    fix: implement Sequential Searching Algorithm

--- Fin-Group-Activity-01/sequential_searching.py
+++ Fin-Group-Activity-01/sequential_searching.py
@@ -1,4 +1,4 @@
-#sequential searching algorithm (assigned member: Gerald D. BOniel)
+#sequential searching algorithm (assigned member: Gerald D. Boniel)
 
 
 """
@@ -7,7 +7,7 @@ unlike binary search (with the ordered elements) that halves the array, which is
 So array [0 - nth index] 
 will just iterate each index if it's the element we are searching for and if true then we can return that index. 
 
-as for testing this, I will use random.choice(data)  # which will produce a random number from the random int list
+as for testing this I will use random.choice(data)  # which will produce a random number from the random int list
 """
 
 
@@ -15,12 +15,10 @@ import random
 
 
 def sequential_searching(data: list, element:int) -> int | None:   #index is int and None if the element is not on the list 
-
 	for i in range(len(data)):
 		if data[i] == element:
 			return i
-			
-    return None
+	return None
 
 
 def main():
@@ -28,16 +26,16 @@ def main():
     data = [random.randint(1, 1000000) for _ in range(N)]
     
     element = random.choice(data)  #element I will be using that is on the data
-    
     non_element = -67   #not an element of the list (I just used negative since this is obviosuly not part of the range in data)
     
+
     
     
-    #testing
+    
     #Search an existing elment in the array
     print(f"Number we are looking for is: {element}.")
     index1 = sequential_searching(data, element)
-    print(f"The number {element} is at index {index1}")
+    print(f"The number {element} is at index {index1},")
     
     
     #Search a non element in the array
